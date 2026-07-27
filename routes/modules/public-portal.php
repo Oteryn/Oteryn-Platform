a4681e3bdd97bd20aec8eec04eb09d8706366129
blakinio <75369544+blakinio@users.noreply.github.com>
2026-07-27 16:34:49 +0200

    fix(web): remove public design preview route

--- routes/modules/public-portal.php
+++ routes/modules/public-portal.php
@@ -6,6 +6,5 @@ use App\Http\Controllers\PublicPortal\PublicSitemapController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', PublicHomeController::class)->name('home');
-Route::view('/design/home-v2', 'home-preview')->name('design.home.v2');
 Route::get('/sitemap.xml', PublicSitemapController::class)->name('seo.sitemap');
 Route::get('/robots.txt', PublicRobotsController::class)->name('seo.robots');
