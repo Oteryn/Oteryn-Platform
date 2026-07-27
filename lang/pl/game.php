d650686dca83b3307cb12631a20167df3f14b181
blakinio <75369544+blakinio@users.noreply.github.com>
2026-07-27 16:29:40 +0200

    feat(game): add Polish vocation labels

---
+++ lang/pl/game.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'vocations' => [
+        'none' => 'Brak profesji',
+        'sorcerer' => 'Sorcerer',
+        'druid' => 'Druid',
+        'paladin' => 'Paladin',
+        'knight' => 'Knight',
+        'master_sorcerer' => 'Master Sorcerer',
+        'elder_druid' => 'Elder Druid',
+        'royal_paladin' => 'Royal Paladin',
+        'elite_knight' => 'Elite Knight',
+        'monk' => 'Monk',
+        'exalted_monk' => 'Exalted Monk',
+        'unknown' => 'Nieznana profesja (ID :id)',
+    ],
+];
