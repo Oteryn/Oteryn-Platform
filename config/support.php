613570248f25278d138da6f53089ffbb08d43c83
blakinio <75369544+blakinio@users.noreply.github.com>
2026-07-28 23:10:20 +0200

    feat(support): configure lifecycle limits and retention

--- config/support.php
+++ config/support.php
@@ -18,4 +18,26 @@ return [
     'contact_email' => env('OTERYN_SUPPORT_CONTACT_EMAIL'),
     'support_url' => env('OTERYN_SUPPORT_URL'),
     'allowed_hosts' => $hostList(env('OTERYN_SUPPORT_ALLOWED_HOSTS')),
+
+    'tickets' => [
+        'subject_max_length' => 160,
+        'message_max_length' => 8000,
+        'open_limit_per_identity' => 10,
+        'retention_days_after_close' => 730,
+    ],
+    'reports' => [
+        'target_max_length' => 160,
+        'evidence_max_length' => 4000,
+        'pending_limit_per_identity' => 5,
+        'retention_days_after_close' => 730,
+    ],
+    'enforcement' => [
+        'reason_max_length' => 4000,
+        'appeal_max_length' => 4000,
+        'retention_days_after_expiry' => 1095,
+    ],
+    'attachments' => [
+        'enabled' => false,
+        'reason' => 'No reviewed support-attachment upload model is currently delivered.',
+    ],
 ];
