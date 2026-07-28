e668f1df6b22fdb1dd01808f27e8035c9e36248d
blakinio <75369544+blakinio@users.noreply.github.com>
2026-07-28 16:19:00 +0200

    feat(identity): register account security provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Providers\AccountSecurityServiceProvider;
 use App\Providers\AppServiceProvider;
 
 return [
     AppServiceProvider::class,
+    AccountSecurityServiceProvider::class,
 ];
