e1194eca140382bb68d204205a6d7b11017a55da
blakinio <75369544+blakinio@users.noreply.github.com>
2026-07-28 10:15:22 +0200

    feat(marketplace): add safe production activation flag

--- config/marketplace.php
+++ config/marketplace.php
@@ -1,6 +1,9 @@
 <?php
 
+$defaultEnabled = env('APP_ENV', 'production') !== 'production';
+
 return [
+    'enabled' => (bool) env('MARKETPLACE_ENABLED', $defaultEnabled),
     'currency_name' => 'Oteryn Coins',
     'escrow_canary_account_id' => (int) env('MARKETPLACE_ESCROW_CANARY_ACCOUNT_ID', 0),
     'allowed_duration_days' => [1, 3, 7],
