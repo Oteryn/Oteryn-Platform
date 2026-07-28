cb45bb5737af4496698eb8981e14009f73e64b58
blakinio <75369544+blakinio@users.noreply.github.com>
2026-07-28 11:21:19 +0200

    feat(catalog): add import result DTO

---
+++ app/GameCatalog/Application/Import/CatalogImportResult.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\GameCatalog\Application\Import;
+
+final readonly class CatalogImportResult
+{
+    public function __construct(
+        public int $snapshotId,
+        public int $importRunId,
+        public string $contentSha256,
+        public bool $deduplicated,
+    ) {}
+}
