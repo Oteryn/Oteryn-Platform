9c0113a0917f64a1307301ff9b42a64004b2dff6
blakinio <75369544+blakinio@users.noreply.github.com>
2026-08-04 23:03:18 +0200

    test(gateway): share runtime schema identity constant

--- services/game-gateway/internal/gateway/schema_hash_test.go
+++ services/game-gateway/internal/gateway/schema_hash_test.go
@@ -7,8 +7,6 @@ import (
 	"testing"
 )
 
-const canonicalNativeSchemaSHA256 = "c7665223f09001e3294e9a03ab4784defed66b0ac04450e8679d4778421207f8"
-
 func TestCanonicalNativeSchemaSHA256(t *testing.T) {
 	data, err := os.ReadFile("../../../../docs/contracts/oteryn_native_gameplay_v1.proto")
 	if err != nil {
